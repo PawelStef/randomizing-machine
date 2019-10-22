6c7bfc18cd1c4bf7b338d12cd4438e30d139b9f0
Paweł Stefanowski <paw.stefanowski@gmail.com>
2019-10-22 18:45:08 +0200

    ad

--- src/main/java/com/j25/cloud/randomizingmachine/RandomizingMachineApplication.java
+++ src/main/java/com/j25/cloud/randomizingmachine/RandomizingMachineApplication.java
@@ -3,7 +3,9 @@ package com.j25.cloud.randomizingmachine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class RandomizingMachineApplication {
 
     public static void main(String[] args) {
